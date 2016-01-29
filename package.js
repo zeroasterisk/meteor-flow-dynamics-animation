cea9484da05da97f2dc6658a21266d21b9280794
alan bount <alan@zeroasterisk.com>
2016-01-29 09:32:25 -0500

    add missing packages.json

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'zeroasterisk:flow-dynamics-animation',
-  version: '0.0.3',
+  version: '0.0.4',
   // Brief, one-line summary of the package.
   summary: 'Simple transitions/animations for FlowRouter using DynamicsJS',
   // URL to the Git repository containing the source code for this package.
